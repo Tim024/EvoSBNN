0dfabc47562539aebf4fa6a7099a8c74b125ec1c
T. van der Lee <t.lee@tue.nl>
2019-09-20 19:58:58 +0200

    improved network

--- SBNN.py
+++ SBNN.py
@@ -3,18 +3,32 @@ import numpy as np
 
 class SparseBinaryLayer():
     def __init__(self,inputs,outputs,density=0.1):
-        super(SparseBinaryLayer, self).__init__()
 
         #Initialize a sparse weight layer completely randomly
-        self.weights = np.array(np.zeros((inputs, outputs)),dtype = bool)
+        self.weights = np.array(np.zeros((outputs, inputs)),dtype=bool)
         for x in range(inputs):
             for y in range(outputs):
                 r = np.random.random()
                 if r < density:
-                    self.weights[x, y] = 1
+                    self.weights[y, x] = 1
 
-        print(self.weights)
+    def forward(self,input):
+        return np.bitwise_and(input,self.weights)
+
+
+class SparseBinaryNeuralNetwork():
+    def __init__(self,I,H,O):
+        self.input_size = I
+        self.l1 = SparseBinaryLayer(I,H)
+        self.l2 = SparseBinaryLayer(H,O)
+
+    def forward(self,input):
+        return self.l2.forward(self.l1.forward(input))
+
 
 
 if __name__=='__main__':
     SL = SparseBinaryLayer(5,10)
+    print(SL.weights)
+    input = np.array([[True,True,True,False,False]])
+    print(SL.forward(input))
